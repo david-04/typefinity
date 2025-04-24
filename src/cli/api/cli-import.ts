88d5cfc5812c79384be3d72d6accdb27c304f258
David Hofmann <65802756+david-04@users.noreply.github.com>
2025-04-24 23:35:46 +0100

    Add intermediate import/re-export module for VSCode import suggestions

---
+++ src/cli/api/cli-import.ts
@@ -0,0 +1,4 @@
+export * from "../../core/api/core-api-private.js";
+export * from "../../core/api/core-api-public.js";
+export * from "../../test/api/test-api-private.js";
+export * from "../../test/api/test-api-public.js";
---
+++ src/core/api/core-import.ts
@@ -0,0 +1,2 @@
+export * from "../../test/api/test-api-private.js";
+export * from "../../test/api/test-api-public.js";
